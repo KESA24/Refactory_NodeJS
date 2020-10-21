131c6934383ab29d1f9f0d415c0a8b1490ec5860
Kesa <nekepatri@gmail.com>
2020-10-21 09:51:43 +0300

    started on routing on pages

--- helloworld/index.js
+++ helloworld/index.js
@@ -5,5 +5,16 @@ const  express = require ('express');
 //Creating an express application by calling the express() function
 const app = express();
 
-//Created a server
+//specify what to do when the user hits the '/'(home page) route endpoint
+app.get("/" , (req, res) => {
+    res.send("Homepage!Hello world, We did it Kesaaa")
+})
+
+//specify what to do when user hits the "/about" route/endpoint
+app.get( '/about' , (req,res)=>{
+    res.send('This is the about page')
+})
+
+
+//Created a server and have it listen on port 3000
 app.listen(3000, () => console.log("Listening on port 3000"))
