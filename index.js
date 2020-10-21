f83d2d075dd11ff2f64bfc0de0f43e1e9182915a
Kesa <nekepatri@gmail.com>
2020-10-21 09:09:18 +0300

    Run a server on my pc

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
 //Importing express in our project using require keyword
 const  express = require ('express');
-//Creating an express application
+
+//Creating an express application by calling the express() function
 const app = express();
+
+//Created a server
+app.listen(3000, () => console.log("Listening on port 3000"))
