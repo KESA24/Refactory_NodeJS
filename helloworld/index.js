704089ef88c546a1b287ee7b5cce407b463dae84
Kesa <nekepatri@gmail.com>
2020-11-08 16:20:19 +0300

    Delete and search mechanism

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,71 +1,63 @@
 // Dependencies
 const express = require("express"); 
-
 const mongoose = require("mongoose"); 
-
 const bodyParser = require("body-parser");
-
 const path = require("path"); //Pug
-
 require("dotenv/config"); //Db hidden
 
 //Routes Folders
-const indexRoutes = require("./routes/indexroutesJA");
-const routes = require("./routes/routes");
-
-
+const indexRoutes = require("./routes/indexroutesJA"); //Classroutes
+const routes = require("./routes/routes"); //Assignmentroutes
 const app = express(); //Execute express by calling the express() function and assign it to the variable "app"
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json()); //Middleware:Use the body-parser
 
-//Db connection
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-});
-
-mongoose.connection
-  .on('open', () => {
-    console.log('Mongoose connection open');
-  })
-  .on('error', (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
 
   // Configurations
-app.set("view engine", "pug"); 
-app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "pug"); 
+  app.set("views", path.join(__dirname, "views"));
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json()); //Middleware:Use the body-parser
 
-app.get("/htmlfile", (req, res) => {
-  res.sendFile(__dirname + "/index.html"); //Return an HTML file
-});
+  // Use the imported routes
+  app.use('/', indexRoutes);
+  app.use(routes);
+  
+  //Return an HTML file
+  app.get("/htmlfile", (req, res) => {
+    res.sendFile(__dirname + "/index.html"); 
+  });
+  
+  //Simple request time logger for a specific route
+  app.use((req, res, next) => {
+      console.log('A new request received at ' + Date.now());
+      next();
+  });
+  
+  //Error Page for non existent routes requests to the server.
+  app.get('*',(req,res)=>{
+      res.send('error page')
+  })
+  
+  //Connection to public folder
+  app.use(express.static(path.join(__dirname, 'public')));
+  
 
+//Db connection
+  mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
 
-//Simple request time logger for a specific route
-app.use((req, res, next) => {
-    console.log('A new request received at ' + Date.now());
-    next();
-});
+  mongoose.connection
+    .on('open', () => {
+      console.log('Mongoose connection open');
+    })
+    .on('error', (err) => {
+      console.log(`Connection error: ${err.message}`);
+    });
 
-
-
-app.use('/', indexRoutes);
-app.use(routes);
-
-app.get('*',(req,res)=>{
-    res.send('error page')
-})
-
-
-
-
-//Connection to public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-app.listen(3000, () => console.log("Listening on port 3000")); //Created a server and have it listen on port 3000
+//Created a server and have it listen on port 5000
+app.listen(5000, () => console.log("Listening on port 5000")); 
 
 
 
--- helloworld/todays updates 0611/indexroutes (2).js
+++ helloworld/todays updates 0611/indexroutes (2).js
@@ -1,55 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
-const Registration = require('../models/registration')
+const Registration = require('../models/Registration')
 
 // specify what to do when user hit the '/'(home page) route/endpoint
-
-//Get the form
-router.get('/', (req,res)=>{
-    res.render('formJA', { title: 'Registration form' })
+router.get('/', (req, res) => {
+    res.render('form', { title: 'Registration form' })
 })
 
-//Send/save data to the database
-
-// router.post('/', (req,res)=>{
-//    console.log(req.body);
-
-//    const registration = new Registration(req.body);
-//          registration.save()
-//         .then(() => { res.send('Thank you for your registration!'); })
-//         .catch((err) => {
-//             console.log(err);
-//             res.send('Sorry! Something went wrong.');
-//     });
-
-//       //res.render('formJA', { title: 'Registration form' })
-// })
-
-// Asynchronous,save data to the database
-router.post('/', async(req,res)=>{
-    try{
+// save data to the database
+router.post('/', async (req, res) => {
+    try {
         const registration = new Registration(req.body);
         await registration.save(() => {
             console.log('save success')
             res.redirect('/userlist')
         })
     }
-    catch(err) {
+    catch (err) {
         res.status(400).send('Sorry! Something went wrong.')
         console.log(err)
-    }   
+    }
 })
 
 // retrieve data from the database 
 router.get('/userlist', async (req, res) => {
     try {
         let items = await Registration.find()
-
-// Search Data in the datatbase   
-
-        if (req.query.name) {
-            items = await Registration.find({ name: req.query.name })
+ //Search the database
+        if (req.query.gender) {
+            items = await Registration.find({ gender: req.query.gender })
         }
         res.render('list', { title: 'User list', users: items })
     } catch (err) {
@@ -57,18 +36,16 @@ router.get('/userlist', async (req, res) => {
     }
 })
 
-//Delete Data from the database
-
-router.post('/delete', async(req,res)=>{
-    try{
-        await Registration.deleteOne({_id: req.body.id })
+// Delete
+router.post('/delete', async (req, res) => {
+    try {
+        await Registration.deleteOne({ _id: req.body.id })
         res.redirect('back')
-    }catch(err){
+    } catch (err) {
         res.status(400).send("Unable to delete item in the database");
-    }})
+    }
+})
 
-
-//Update
 router.get('/update/:id', async (req, res) => {
     try {
         const updateUser = await Registration.findOne({ _id:req.params.id })
@@ -86,6 +63,4 @@ router.post('/update', async (req, res) => {
         res.status(404).send("Unable to update item in the database");
     }    
 })
-
-
 module.exports = router;
